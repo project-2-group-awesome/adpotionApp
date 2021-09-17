bbf287639745c587a328fca437607366b5537f1b
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2021-09-17 11:14:30 -0700

    added doc type, linked styles and app & and wrote some sudo code for htim and app.

---
+++ app.js
@@ -0,0 +1,8 @@
+// Set up namespace object
+// Fetch Api data
+// Add event listener to listen for users animal type choice
+// Display user choice on page (create li element and append to ul)
+
+// Add like button to animal profiles
+// Add event listener to auto scroll to next animal profile once like button has been pushed
+// save user like data in favourites section
