142f0ba0aa888caf1e8c80e5e59ae2679811f349
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2021-09-17 18:54:11 -0700

    typo fix

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // Add event listener to listen for users animal type choice
 // Display user choice on page (create li element and append to ul)
 
+
+
 // Add like button to animal profiles
 // Add event listener to auto scroll to next animal profile once like button has been pushed
-// save user like data in favourites section
+// save user like data in favourites section.
