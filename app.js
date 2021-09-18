f97f3fd1c7713dee351530fb2bfe76ab6d6c11a8
Seungmin <shin891211@gmail.com>
2021-09-17 21:45:35 -0400

    added html and styles

--- app.js
+++ app.js
@@ -6,3 +6,5 @@
 // Add like button to animal profiles
 // Add event listener to auto scroll to next animal profile once like button has been pushed
 // save user like data in favourites section
+
+const app = {};
