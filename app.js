2d9e6297c77859080de19dba5c61abb512a75356
Seungmin <shin891211@gmail.com>
2021-09-19 17:02:19 -0400

    added api call (need to be re-formated)

--- app.js
+++ app.js
@@ -13,51 +13,73 @@
 
 const adoptionApp = {};
 adoptionApp.apiKey = 'PjoCn4l5'
-adoptionApp.animalTypes = ['alpacas',
-    'birds',
-    'cats',
-    'chickens',
-    'chinchillas',
-    'cows',
-    'degus',
-    'dogs',
-    'donkeys',
-    'ducks',
-    'ferrets',
-    'fish',
-    'frogs',
-    'geckos',
-    'geese',
-    'gerbils',
-    'goats',
-    'guineapigs',
-    'hamsters',
-    'hedgehogs',
-    'horses',
-    'iguanas',
-    'lizards',
-    'llama',
-    'mice',
-    'pigs',
-    'ponies',
-    'rabbits',
-    'rats',
-    'sheep',
-    'skunks',
-    'snakes',
-    'tortoises',
-    'turkeys',
-    'turtles'];
+// adoptionApp.animalTypes = ['alpacas',
+//     'birds',
+//     'cats',
+//     'chickens',
+//     'chinchillas',
+//     'cows',
+//     'degus',
+//     'dogs',
+//     'donkeys',
+//     'ducks',
+//     'ferrets',
+//     'fish',
+//     'frogs',
+//     'geckos',
+//     'geese',
+//     'gerbils',
+//     'goats',
+//     'guineapigs',
+//     'hamsters',
+//     'hedgehogs',
+//     'horses',
+//     'iguanas',
+//     'lizards',
+//     'llama',
+//     'mice',
+//     'pigs',
+//     'ponies',
+//     'rabbits',
+//     'rats',
+//     'sheep',
+//     'skunks',
+//     'snakes',
+//     'tortoises',
+//     'turkeys',
+//     'turtles'];
 
-adoptionApp.userOptions = (array) => {
-    const animalList = document.querySelector('#animalList');
-
-    array.forEach((item) => {
-        animalList.innerHTML += `
-            <option value="${item}">${item}</option>
-        `
+adoptionApp.getAnimalsName = () => {
+    const url = new URL(`https://api.rescuegroups.org/v5/public/animals/species/`)
+    fetch(url, {
+        headers: {
+            'Authorization': adoptionApp.apiKey
+        }
     })
+        .then(res => res.json())
+        .then((apiInfo) => {
+            const animalList = document.querySelector('#animalList');
+            let species = "";
+            // console.log(data.attributes.singular)
+            apiInfo.data.forEach((data) => {
+                species += `<option value="${data.attributes.plural}">${data.attributes.plural}</option>`
+            })
+            animalList.innerHTML = species.toLowerCase();
+            // console.log(species);
+
+        })
+
 }
+
+// adoptionApp.userOptions = (array) => {
+//     const animalList = document.querySelector('#animalList');
+
+//     array.forEach((item) => {
+//         animalList.innerHTML += `
+//             <option value="${item}">${item}</option>
+//         `
+//     })
+// }
 
 adoptionApp.getData = (choice) => {
     const url = new URL(`https://api.rescuegroups.org/v5/public/animals/search/${choice}/`)
@@ -78,11 +100,13 @@ adoptionApp.display = (dataFromApi) => {
     const ul = document.querySelector('.data-display');
     ul.innerHTML = "";
     dataFromApi.forEach((res) => {
+        console.log(res.attributes.descriptionText);
         const li = document.createElement('li')
         ul.appendChild(li);
         const picture = res.attributes.pictureThumbnailUrl;
         const name = res.attributes.name
         const description = res.attributes.descriptionText
+
         li.innerHTML = `
             <div>
                 <img src="${picture}"/>
@@ -96,9 +120,10 @@ adoptionApp.display = (dataFromApi) => {
 }
 
 adoptionApp.userSelection = () => {
-    const button = document.querySelector('form');
-    button.addEventListener('submit', (e) => {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
         e.preventDefault();
+        console.log(e);
         const userAnimalChoice = e.target.animalList.value;
         adoptionApp.getData(userAnimalChoice);
     })
@@ -107,7 +132,8 @@ adoptionApp.userSelection = () => {
 
 
 adoptionApp.init = () => {
-    adoptionApp.userOptions(adoptionApp.animalTypes);
+    adoptionApp.getAnimalsName();
+    // adoptionApp.userOptions(adoptionApp.animalTypes);
     adoptionApp.userSelection();
 
 };
